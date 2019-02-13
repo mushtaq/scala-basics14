c9e9d2aa5519ef9994eeda3ee7aade628225651b
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-02-13 11:14:59 +0530

    Init

---
+++ project/Dependencies.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+object Dependencies {
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.6-SNAP6"
+  val `play-json` = "com.typesafe.play" %% "play-json" % "2.7.1"
+}
---
+++ src/main/scala/data/JsDemo.scala
@@ -0,0 +1,7 @@
+package data
+
+import play.api.libs.json._
+
+object JsDemo {
+  def transform(json: JsValue): JsValue = ???
+}
